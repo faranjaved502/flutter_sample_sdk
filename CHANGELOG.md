07303d379e83a6fb87658d0129f5d9f82931fa65
faran.javed <faran.javed@arq.group>
2023-11-29 15:39:25 +1100

    Add sample project

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,3 +1,4 @@
-## 0.0.1
 
-* TODO: Describe initial release.
+
+## [version] - Date
+* explanations about changes.
---
+++ example/android/app/src/main/kotlin/com/flutter_sample_sdk/example/example/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.flutter_sample_sdk.example.example
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
